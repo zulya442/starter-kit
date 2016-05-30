c07601f062101ec49201b1504bbd355c69149e59
Zulya Dzulkefli <zelia442@gmail.com>
2016-05-31 10:32:12 +1200

    added flikity carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 var responsiveNav = require('responsive-nav');
+var Flickity = require('flickity');
 
 document.addEventListener('DOMContentLoaded', function(){
 
+//Scripts
 	var nav = responsiveNav('.nav-collapse', {
 		transition: 400,
 		insert: 'after'
 	
 	});
 
-	alert('hello');
+	var flickity = new Flickity('.flickity', {
+		contain: true,
+		wrapAround: true
+	});
 });
